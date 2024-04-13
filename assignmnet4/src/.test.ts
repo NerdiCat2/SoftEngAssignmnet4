dbcc1bbac8cb4dce87a94050aba019d3d64bbbcf
NerdiCat2 <cdhunt@wpi.edu>
2024-04-13 16:05:32 -0400

    done with pair programing

--- assignmnet4/src/.test.ts
+++ assignmnet4/src/.test.ts
@@ -1,13 +1,66 @@
-import { assert, expect, test} from 'vitest'
+import { expect, test} from 'vitest'
 import { arabic2bumblor } from "./BumblorArabicConverter.ts";
 import { bumblor2arabic } from "./BumblorArabicConverter.ts";
 
 test('Out of bounds error', () => {
-        expect(arabic2bumblor(5000)).toThrow(RangeError("Out of Range"));
-        expect(arabic2bumblor(-5000)).toThrow(RangeError("Out of Range"));
+        expect(() => arabic2bumblor(5000)).toThrow("Out of Range");
+        expect(() => arabic2bumblor(-5000)).toThrow("Out of Range");
+        expect(() => arabic2bumblor(-4999.01)).toThrow("Out of Range");
+        expect(() => arabic2bumblor(4999.01)).toThrow("Out of Range");
+        expect(() => arabic2bumblor(-4999)).not.toThrow("Out of Range");
+        expect(() => arabic2bumblor(4999)).not.toThrow("Out of Range");
     }
 );
 
-test('Out of bounds error', () => {
-    bumblor2arabic("ABC");
+test('Possible strings', () => {
+    expect( arabic2bumblor(1666)).toBe("MDCLXVI");
+    expect( arabic2bumblor(-3240)).toBe("-MMMCCXXXX");
+    expect( arabic2bumblor(-3240.634)).toBe("-MMMCCXXXX");
+    expect( arabic2bumblor(0)).toBe("O");
+});
+
+test('Possible nummbers', () =>{
+    expect(bumblor2arabic("-MMMCCXXXX")).toBe(-3240);
+    expect(bumblor2arabic("MMMCCXXXX")).toBe(3240);
+})
+
+test('Too many of value', () =>{
+    expect(() => bumblor2arabic("MMMMM")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("MMMM")).not.toThrow("Malformed Number");
+    expect(() => bumblor2arabic("CCCCC")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("CCC")).not.toThrow("Malformed Number")
+    expect(() => bumblor2arabic("XXXXX")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("XX")).not.toThrow("Malformed Number")
+    expect(() => bumblor2arabic("IIIII")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("IIII")).not.toThrow("Malformed Number")
+    expect(() => bumblor2arabic("DD")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("D")).not.toThrow("Malformed Number");
+    expect(() => bumblor2arabic("LL")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("L")).not.toThrow("Malformed Number");
+    expect(() => bumblor2arabic("VV")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("V")).not.toThrow("Malformed Number");
+})
+test('Only in a row', () => {
+    expect(() => bumblor2arabic("-MMCMCXXX")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("-MXMCCXX")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("-MMCCXXDX")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("-MMMCCXXX")).not.toThrow("Malformed Number");
+})
+
+test('lowerCase', () =>{
+    expect(() => bumblor2arabic("mdclxvi")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("MDCLXVI")).not.toThrow("Malformed Number");
+})
+
+test('0', () => {
+    expect(() => bumblor2arabic("OMDCLXVI")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("OO")).toThrow("Malformed Number");
+    expect( bumblor2arabic("O")).toBe(0);
+})
+
+test("erroneous spaces", () => {
+    expect(() => bumblor2arabic(" MDCLXVI")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("MDCLXVI ")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("MDCLX VI")).toThrow("Malformed Number");
+    expect(() => bumblor2arabic("MDCLXVI")).not.toThrow("Malformed Number");
 })
--- assignmnet4/src/BumblorArabicConverter.ts
+++ assignmnet4/src/BumblorArabicConverter.ts
@@ -1,20 +1,53 @@
-class BumblorArabicConverter {
-    bumblor2arabic(Bumblor: string): number {
-
-        Bumblor;
+//type Bumblor = string;
+export function bumblor2arabic(Bumblor: string): number {
+    if (Bumblor=="O"){
         return 0;
     }
-    arabic2bumblor(arabic: number): string {
-        try{
-            if (arabic<=-5000 || arabic>=5000){
-                throw new RangeError("Out of Range");
-            }
-        }
-        catch (error){
-            console.log(error);
-        }
-        arabic;
-        return "";
+    let sign=1;
+    if (Bumblor.startsWith("-")){
+        Bumblor=Bumblor.slice(1);
+        sign=-1;
+
     }
+    let output =0;
+    const values: {[key: string]:number} = {"M":1000,"D":500,"C":100,"L":50,"X":10,"V":5,"I":1};
+    const regex = /^M{0,4}D{0,1}C{0,4}L{0,1}X{0,4}V{0,1}I{0,4}$/;
+    if (!regex.test(Bumblor)){
+        throw Error("Malformed Number");
+    }
+    Array.from(Bumblor).forEach(char =>{
+        output+=values[char];
+    });
+    Bumblor;
+    return sign*output;
 }
-export default BumblorArabicConverter;
+
+export function arabic2bumblor(arabic: number):string {
+    let output =  "";
+    if (arabic<-4999 || arabic>4999) {
+        throw Error("Out of Range");
+    }
+    if (arabic==0){
+        return "O";
+    }
+    if (arabic<0){
+        arabic=arabic*-1;
+        output="-";
+    }
+    const M=Math.floor(arabic/1000);
+    arabic-=1000*M;
+    const D =Math.floor(arabic/500);
+    arabic-=500*D;
+    const C =Math.floor(arabic/100);
+    arabic-=100*C;
+    const L=Math.floor(arabic/50);
+    arabic-=50*L;
+    const X =Math.floor(arabic/10);
+    arabic-=10*X;
+    const V =Math.floor(arabic/5);
+    arabic-=5*V;
+    const I =Math.floor(arabic);
+    arabic-=I;
+    output+="M".repeat(M) + "D".repeat(D) + "C".repeat(C) + "L".repeat(L) + "X".repeat(X) + "V".repeat(V) + "I".repeat(I);
+    return output;
+}
