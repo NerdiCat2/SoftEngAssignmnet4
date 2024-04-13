44b6f18390632d28da02b44404fb8c187e056845
NerdiCat2 <cdhunt@wpi.edu>
2024-04-13 13:25:31 -0400

    added function stubs

---
+++ assignmnet4/src/BumblorArabicConverter.ts
@@ -0,0 +1,10 @@
+type Bumblor = string;
+export function bumblor2arabic(Bumblor: Bumblor): number {
+    Bumblor;
+    return 0;
+}
+
+export function arabic2bumblor(arabic: number): Bumblor {
+    arabic;
+    return "";
+}
