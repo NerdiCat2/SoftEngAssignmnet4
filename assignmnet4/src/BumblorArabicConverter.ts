ac4665127da7dd1c9423f1dd586e4d2de46b7976
NerdiCat2 <cdhunt@wpi.edu>
2024-04-13 13:39:00 -0400

    OutofRangeError

--- assignmnet4/src/BumblorArabicConverter.ts
+++ assignmnet4/src/BumblorArabicConverter.ts
@@ -1,10 +1,19 @@
 type Bumblor = string;
 export function bumblor2arabic(Bumblor: Bumblor): number {
+
     Bumblor;
     return 0;
 }
 
 export function arabic2bumblor(arabic: number): Bumblor {
+    try{
+        if (arabic<=-5000 || arabic>=5000){
+            throw new RangeError("Out of Range");
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
     arabic;
     return "";
 }
